c470b985b04f21621aada54280365823e014be08
Kahyberth <kahyberth@gmail.com>
2025-05-24 03:12:15 -0500

    refactor: replace hardcoded database configuration with environment variables and add SSL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstimationService } from './estimation/estimation.service';
 import { PokerWsModule } from './poker-ws/poker-ws.module';
 import { PokerModule } from './poker/poker.module';
+import { envs } from './commons/envs';
 
 @Module({
   imports: [
@@ -14,12 +15,17 @@ import { PokerModule } from './poker/poker.module';
     PokerModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'poker',
+      host: envs.DB_HOST,
+      port: envs.DB_PORT,
+      username: envs.DB_USERNAME,
+      password: envs.DB_PASSWORD,
+      database: envs.DB_DATABASE,
       synchronize: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
     }),
   ],
